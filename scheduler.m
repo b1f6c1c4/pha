4386fccbbbccedc3b4463130f0540b4a9f030087
b1f6c1c4 <b1f6c1c4@gmail.com>
2017-01-16 23:25:12 +0800

    more sch

--- scheduler.m
+++ scheduler.m
@@ -18,24 +18,79 @@ h0Trig = h0Trig ./ sum(h0Trig);
 %% First-order differential
 h1Naive = [1,0,-1].*(fs/(4*pi));
 
+%% Actual data
+acData = load('WavData.mat');
+
+%% Prepare
+format short;
+!copy /b /y NUL "./Generated/data.txt"
+diary('./Generated/data.txt');
+
 %% h0 = Rect, h1 = Naive
+disp('=============');
+disp('h0=Rect, h1=Naive:');
 test(h0Rect, h1Naive, 'rect-naive');
+
+%% h0 = Trig, h1 = Naive
+disp('=============');
+disp('h0=Trig, h1=Naive:');
+test(h0Trig, h1Naive, 'trig-naive');
+
+diary off;
 
 %% Subfunctions
 
 function []=test(h0, h1, plotting)
 
-    %% Sine wave, df = 0
-    df   = 0;
     A0   = 1;
     phi0 = rand(1,1) * pi;
     ka   = 0;
     kx   = 0;
     fm   = 5;
 
+    %% Sine wave, df = 0
+    df   = 0;
     [Data, standard] = gen(f0, fs, L, df, A0, phi0, ka, kx, fm);
     [estimated]      = pmu(f0, fs, Data, h0, h1);
     [TVE, FE, RFE]   = err(fs, length(h0), length(h1), standard, estimated, [plotting '-s0']);
+    disp(['df=0: TVE=' num2str(TVE*100) '% FE=' num2str(FE) 'Hz RFE=' num2str(RFE) 'Hz/s']);
+
+    %% Sine wave, df = +1
+    df   = +1;
+    [Data, standard] = gen(f0, fs, L, df, A0, phi0, ka, kx, fm);
+    [estimated]      = pmu(f0, fs, Data, h0, h1);
+    [TVE, FE, RFE]   = err(fs, length(h0), length(h1), standard, estimated, [plotting '-sp1']);
+    disp(['df=+1: TVE=' num2str(TVE*100) '% FE=' num2str(FE) 'Hz RFE=' num2str(RFE) 'Hz/s']);
+
+    %% Sine wave, df = -1
+    df   = -1;
+    [Data, standard] = gen(f0, fs, L, df, A0, phi0, ka, kx, fm);
+    [estimated]      = pmu(f0, fs, Data, h0, h1);
+    [TVE, FE, RFE]   = err(fs, length(h0), length(h1), standard, estimated, [plotting '-sm1']);
+    disp(['df=-1: TVE=' num2str(TVE*100) '% FE=' num2str(FE) 'Hz RFE=' num2str(RFE) 'Hz/s']);
+
+    %% Sine wave, df = +2
+    df   = +2;
+    [Data, standard] = gen(f0, fs, L, df, A0, phi0, ka, kx, fm);
+    [estimated]      = pmu(f0, fs, Data, h0, h1);
+    [TVE, FE, RFE]   = err(fs, length(h0), length(h1), standard, estimated, [plotting '-sp2']);
+    disp(['df=+2: TVE=' num2str(TVE*100) '% FE=' num2str(FE) 'Hz RFE=' num2str(RFE) 'Hz/s']);
+
+    %% Sine wave, df = -2
+    df   = -2;
+    [Data, standard] = gen(f0, fs, L, df, A0, phi0, ka, kx, fm);
+    [estimated]      = pmu(f0, fs, Data, h0, h1);
+    [TVE, FE, RFE]   = err(fs, length(h0), length(h1), standard, estimated, [plotting '-sm2']);
+    disp(['df=-2: TVE=' num2str(TVE*100) '% FE=' num2str(FE) 'Hz RFE=' num2str(RFE) 'Hz/s']);
+
+    %% AM-FM
+    df   = 0;
+    ka   = 0.1;
+    kx   = 0.1;
+    [Data, standard] = gen(f0, fs, L, df, A0, phi0, ka, kx, fm);
+    [estimated]      = pmu(f0, fs, Data, h0, h1);
+    [TVE, FE, RFE]   = err(fs, length(h0), length(h1), standard, estimated, [plotting '-amfm']);
+    disp(['AM-FM: TVE=' num2str(TVE*100) '% FE=' num2str(FE) 'Hz RFE=' num2str(RFE) 'Hz/s']);
 
 end
 
